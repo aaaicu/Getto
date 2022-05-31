ab5b5113b306f0f26f579d923747e95477249357
Jaehyun <aaaicu@zuminternet.com>
2022-05-31 14:25:32 +0900

    Init Commit

---
+++ src/main/java/com/yummythings/getto/GettoApplication.java
@@ -0,0 +1,13 @@
+package com.yummythings.getto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yummythings/getto/GettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yummythings.getto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
