84096b2a4a654b4bf8f5bb377e6a3a887bb65a82
aaaicu <aaaaicu@gmail.com>
2022-06-02 01:14:11 +0900

    YMAL 암호화 처리

---
+++ src/test/java/com/yummythings/getto/encrypt/JasyptTest.java
@@ -0,0 +1,46 @@
+package com.yummythings.getto.encrypt;
+
+import org.assertj.core.api.Assertions;
+import org.jasypt.encryption.pbe.StandardPBEByteEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+public class JasyptTest {
+
+    @Test
+    void 암호화테스트(){
+        String url = "jdbc:oracle:thin:@test_high?TNS_ADMIN=/test/Wallet_TestDB";
+        String username = "username";
+        String password = "password";
+
+        final String KEY = "test";
+        String encryptUrl = jasyptEncrypt(url, KEY);
+        String encryptUsername = jasyptEncrypt(username, KEY);
+        String encryptPassword = jasyptEncrypt(password, KEY);
+
+        System.out.println("encryptUrl : " + encryptUrl);
+        System.out.println("encryptUsername : " + encryptUsername);
+        System.out.println("encryptPassword : " + encryptPassword);
+
+        Assertions.assertThat(url).isEqualTo(jasyptDecrypt(encryptUrl, KEY));
+        Assertions.assertThat(username).isEqualTo(jasyptDecrypt(encryptUsername, KEY));
+        Assertions.assertThat(password).isEqualTo(jasyptDecrypt(encryptPassword, KEY));
+    }
+
+    private String jasyptEncrypt(String input, String key) {
+        StandardPBEStringEncryptor encryptor = getStandardPBEStringEncryptor(key);
+        return encryptor.encrypt(input);
+    }
+
+    private String jasyptDecrypt(String input, String key){
+        StandardPBEStringEncryptor encryptor = getStandardPBEStringEncryptor(key);
+        return encryptor.decrypt(input);
+    }
+
+    private StandardPBEStringEncryptor getStandardPBEStringEncryptor(String key) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm(StandardPBEByteEncryptor.DEFAULT_ALGORITHM);
+        encryptor.setPassword(key);
+        return encryptor;
+    }
+}
