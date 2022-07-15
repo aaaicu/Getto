df50045129ad9e444621977328aa41666d616a99
aaaicu <aaaaicu@gmail.com>
2022-07-15 18:16:00 +0900

    bugfix : CORS_ALLOW_URL_PATTERNS 으로 수정

--- src/main/java/com/yummythings/getto/endpoint/oauth/OAuthController.java
+++ src/main/java/com/yummythings/getto/endpoint/oauth/OAuthController.java
@@ -22,7 +22,7 @@ public class OAuthController {
     private final JwtService jwtService;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    @CrossOrigin(origins = AccessableClient.CORS_ALLOW_URL)
+    @CrossOrigin(originPatterns = AccessableClient.CORS_ALLOW_URL_PATTERNS)
     @ResponseBody
     @GetMapping("/kakao")
     public ResponseEntity<TokenDTO> kakaoCallback(@RequestParam String code) {
