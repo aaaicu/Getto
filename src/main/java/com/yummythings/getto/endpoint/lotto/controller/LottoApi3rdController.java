6823e9c60c27592adf176e81a9801c8b8f07f9bf
aaaicu <aaaaicu@gmail.com>
2022-08-18 22:46:19 +0900

    modify: 로또번호 조회 저장 캐시기간 수정

--- src/main/java/com/yummythings/getto/endpoint/lotto/controller/LottoApi3rdController.java
+++ src/main/java/com/yummythings/getto/endpoint/lotto/controller/LottoApi3rdController.java
@@ -1,8 +1,10 @@
 package com.yummythings.getto.endpoint.lotto.controller;
 
+import com.yummythings.getto.domain.LuckyNumber;
 import com.yummythings.getto.endpoint.lotto.facade.LottoApi3rdFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,9 @@ public class LottoApi3rdController {
     private final LottoApi3rdFacade lottoApi3rdFacade;
 
     @GetMapping("/last/lucky-number")
-    public String lastLuckyNumber() {
-
-        return lottoApi3rdFacade.getLastLuckyNumber().toString();
+    public ResponseEntity<LuckyNumber> lastLuckyNumber() {
+        return ResponseEntity.ok(lottoApi3rdFacade.getLastLuckyNumber());
     }
-
 
     @GetMapping("/last/lucky-number/refresh")
     public void refreshCachedLuckyNumber() {
